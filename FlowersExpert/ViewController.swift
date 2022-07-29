dfcbea7881964eb92e62bfbccb5cbfa2401ce7f9
Gil Marom <000gil@gmail.com>
2022-07-29 15:44:02 +0300

    Set up UIImagePickerController

--- FlowersExpert/ViewController.swift
+++ FlowersExpert/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var flowerImageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
     }
-
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
 
 }
 
+//MARK: - UIImagePickerControllerDelegate
+    
+    extension ViewController: UIImagePickerControllerDelegate {
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
+            if let userPickedImage = info[.originalImage] as? UIImage {
+                flowerImageView.image = userPickedImage
+            }
+            
+            imagePicker.dismiss(animated: true)
+            
+        }
+        
+    }
